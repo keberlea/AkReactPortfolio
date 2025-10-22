06c527abd5543321ae843cce3f07c8859e15c914
Emojin <emojin.team@gmail.com>
2025-10-22 13:00:48 -0700

    Updated styling with natural colors

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import resumePDF from '../assests/resume25.pdf';
 
 function Resume() {
     const frontEndSkills = [
@@ -28,41 +29,62 @@ function Resume() {
         'Progressive Web Applications (PWA)',
         'State (in the context of React)'
       ];
-return(
-<section id="resume">
-    <h2 className="page-title">Tech Resume</h2>
-        <section className="body-text">
-            <h4>Front-End Development</h4>
-            <ul>
-            {frontEndSkills.map((skill, index) => (
-                <li key={`frontEndSkill-${index}`}>{skill}</li>
-            ))}
-            </ul>
 
-            <h4>Back-End Development</h4>
-            <ul>
-            {backEndSkills.map((skill, index) => (
-                <li key={`backEndSkill-${index}`}>{skill}</li>
-            ))}
-            </ul>
+    return(
+        <section id="resume">
+            <h2 className="page-title">Tech Resume</h2>
+            
+            {/* PDF Download/View Section */}
+            <div className="resume-pdf-section">
+                <a 
+                    href={resumePDF} 
+                    target="_blank" 
+                    rel="noopener noreferrer" 
+                    className="resume-button"
+                >
+                    📄 View Full Resume
+                </a>
+                <a 
+                    href={resumePDF} 
+                    download="Alicia_Keberle_Resume.pdf" 
+                    className="resume-button download"
+                >
+                    ⬇️ Download Resume
+                </a>
+            </div>
 
-            <h4>Web Development Frameworks/Architectures</h4>
-            <ul>
-            {webDevSkills.map((skill, index) => (
-                <li key={`webDevSkill-${index}`}>{skill}</li>
-            ))}
-            </ul>
+            <section className="body-text">
+                <h4>Front-End Development</h4>
+                <ul>
+                {frontEndSkills.map((skill, index) => (
+                    <li key={`frontEndSkill-${index}`}>{skill}</li>
+                ))}
+                </ul>
 
-            <h4>Additional Concepts</h4>
-            <ul>
-            {additionalSkills.map((skill, index) => (
-                <li key={`additionalSkill-${index}`}>{skill}</li>
-            ))}
-            </ul>
+                <h4>Back-End Development</h4>
+                <ul>
+                {backEndSkills.map((skill, index) => (
+                    <li key={`backEndSkill-${index}`}>{skill}</li>
+                ))}
+                </ul>
 
-        </section> 
-</section>
-);
+                <h4>Web Development Frameworks/Architectures</h4>
+                <ul>
+                {webDevSkills.map((skill, index) => (
+                    <li key={`webDevSkill-${index}`}>{skill}</li>
+                ))}
+                </ul>
+
+                <h4>Additional Concepts</h4>
+                <ul>
+                {additionalSkills.map((skill, index) => (
+                    <li key={`additionalSkill-${index}`}>{skill}</li>
+                ))}
+                </ul>
+
+            </section> 
+        </section>
+    );
 }
 
 export default Resume;
